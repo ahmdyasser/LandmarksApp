269bb035a7553b6b5642e32bd522d4ed4e9a29a1
ahmdyasser <ahmadyas_ser@icloud.com>
2022-10-23 11:49:49 +0200

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Ahmad Yasser on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
